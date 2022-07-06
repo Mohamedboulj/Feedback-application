0d8f680f6c4d36ea2a1b00af4fdf33134b89e683
Mohamed Bouljihel <med.blj93@gmail.com>
2022-07-07 00:35:29 +0100

    Delete functionnality

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import FeedbackItem from './FeedbackItem'
+import React from "react";
+import FeedbackItem from "./FeedbackItem";
 
-export default function FeedbackList({feedback,handleClick}) {
-    if(!feedback || feedback.lenght===0){
-       return <div className='p-2'><p>No feedback yet!</p> </div> 
-    }
+export default function FeedbackList({ feedback, handleClick }) {
+  if (!feedback || feedback.lenght === 0) {
+    return (
+      <div className="p-2">
+        <p>No feedback yet!</p>{" "}
+      </div>
+    );
+  }
   return (
-    <div className='feedback-List'>
-        {feedback.map((item)=>
-          (<FeedbackItem key={item.id} item={item} handleClick={handleClick}/>)
-        )}
+    <div className="feedback-List">
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} handleClick={handleClick} />
+      ))}
     </div>
-  )
+  );
 }
