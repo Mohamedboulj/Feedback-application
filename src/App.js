16358498556d13bb99ad4d5afccd0133d7511e99
Mohamed Bouljihel <med.blj93@gmail.com>
2022-07-07 00:35:07 +0100

    Delete function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import  Header  from "./components/Header";
-import  FeedbackItem  from "./components/FeedbackItem";
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/feedbackData";
 
-function App(){
-    return <>
-    <Header/>
-    <div>
-        <FeedbackItem/>
-    </div>
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you wanna delete?')){
+        setFeedback(feedback.filter((item) => item.id !== id))
+    }
+   
+  }
+  return (
+    <>
+      <Header />
+      <div>
+        <FeedbackList feed={feedback} handleClick={deleteFeedback} />
+      </div>
     </>
-     
-    
+  );
 }
 export default App;
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import {useState} from 'react'
-function FeedbackItem() {
-  const [rating,SetRating] = useState(7);
-  const [feed,SetFeed] = useState('default feedback');
+import Card from "./shared/Card"
+import { FaTimes } from "react-icons/fa"
 
+function FeedbackItem({item,handleClick}) {
+ 
 
   return (
-    <div className='card'>
-        <div className='num-display'>{rating}</div>
-        <div className='text-display'>{feed}</div>
-
-    </div>
-  )
+    <Card className="card">
+      <div className="num-display">{item.rating}</div>
+      <button className="close" onClick={()=>handleClick(item.id)}>
+        <FaTimes color='purple' />
+      </button>
+      <div className="text-display">{item.text}</div>
+    </Card>
+  );
 }
 
-export default FeedbackItem
+export default FeedbackItem;
